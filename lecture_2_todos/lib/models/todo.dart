03069e324b42bdce695cc8651d624bf95a9247f4
iMoonkid <32egwt@gmail.com>
2019-10-26 18:25:52 +0300

    #added todo model #minor appearance changes

---
+++ lecture_2_todos/lib/models/todo.dart
@@ -0,0 +1,36 @@
+import 'package:meta/meta.dart';
+
+class Todo {
+  const Todo({
+    @required this.text,
+    @required this.isFinished,
+  });
+
+  final String text;
+  final bool isFinished;
+
+//<editor-fold desc="Data Methods" defaultstate="collapsed">
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is Todo &&
+          runtimeType == other.runtimeType &&
+          text == other.text &&
+          isFinished == other.isFinished);
+
+  @override
+  int get hashCode => text.hashCode ^ isFinished.hashCode;
+
+  Todo copyWith({
+    String text,
+    bool isFinished,
+  }) {
+    return Todo(
+      text: text ?? this.text,
+      isFinished: isFinished ?? this.isFinished,
+    );
+  }
+
+//</editor-fold>
+}
